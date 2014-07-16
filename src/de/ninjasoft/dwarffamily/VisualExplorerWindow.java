bc9bb9d61736a25d1cbaec1024795cc5d136a239
tjohnman <ds_joan@yahoo.es>
2014-07-16 19:05:21 +0200

    Crude and experimental drawing of a dwarfs descendants.

--- src/de/ninjasoft/dwarffamily/VisualExplorerWindow.java
+++ src/de/ninjasoft/dwarffamily/VisualExplorerWindow.java
@@ -17,6 +17,7 @@ import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -49,6 +50,7 @@ public class VisualExplorerWindow extends JFrame implements MouseListener, Mouse
         this.add(drawPanel);
         this.pack();
         drawPanel.centerOnOrigin();
+        drawPanel.plotTree();
         
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
@@ -99,12 +101,26 @@ public class VisualExplorerWindow extends JFrame implements MouseListener, Mouse
 
 class FamilyTreePanel extends JPanel
 {
+    class Rect
+    {
+        public int x, y, width, height;
+        public String str;
+        
+        public Rect(int _x, int _y, int _w, int _h)
+        {
+            x = _x; y = _y; width = _w; height = _h;
+        }
+    }
+    
+    HashMap< Number, ArrayList<Rect> > generations;
+    
     public int camera_x, camera_y;
     Dwarf originDwarf = null;
     ArrayList<Dwarf> dwarfList;
     
     public FamilyTreePanel(ArrayList<Dwarf> dwarves, Dwarf origin)
     {
+        generations = new HashMap<>();
         dwarfList = dwarves;
         originDwarf = origin;
     }
@@ -122,23 +138,118 @@ class FamilyTreePanel extends JPanel
         return new Dimension(800, 600);
     }
     
+    public void plotTree()
+    {
+        Graphics g = getGraphics();
+        if(g == null) return;
+        
+        plotDwarfName(g, originDwarf, 0, 0, 0);
+        plotDwarfDescendancy(g, originDwarf, 0, 0, 0);
+    }
+    
     @Override
     public void paintComponent(Graphics g)
     {
         if(originDwarf == null) return; // Not ready yet.
         
         super.paintComponent(g);
+        FontMetrics fontMetrics = g.getFontMetrics();
         
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(Color.WHITE);
-        drawCenteredString(g, originDwarf.getCasedName(), 0, 0);
+        
+        for(int i=0; i<generations.size(); ++i)
+        {
+            for(int j=0; j<generations.get(i).size(); ++j)
+            {
+                Rect r = generations.get(i).get(j);
+                g.drawRect(r.x - camera_x - 5, r.y - camera_y - 5 - fontMetrics.getAscent(), r.width, r.height);
+                g.drawString(r.str, r.x - camera_x, r.y - camera_y);
+            }
+        }
     }
     
-    private void drawCenteredString(Graphics g, String str, int x, int y)
+    /*private void drawCenteredString(Graphics g, String str, int x, int y)
     {
         FontMetrics fontMetrics = g.getFontMetrics();
         int width = fontMetrics.stringWidth(str);
         g.drawString(str, x-camera_x - width/2, y-camera_y -fontMetrics.getHeight()/2);
+    }*/
+            
+    private Rect plotDwarfName(Graphics g, Dwarf dwarf, int x, int y, int generation)
+    {
+        FontMetrics fontMetrics = g.getFontMetrics();
+        
+        int width = fontMetrics.stringWidth(dwarf.getCasedName()) + 10;
+        int height = fontMetrics.getHeight() + 10;
+        g.setColor(Color.yellow);
+        
+        int drx = x - width/2 - 5;
+        int dry = y - height - 5 - fontMetrics.getAscent()/2;
+        
+        Rect r = new Rect(drx, dry, width, height);
+        r.str = dwarf.getCasedName();
+        
+        while(generationContainsIntersectingRect(generation, r))
+        {
+            r.x += 10;
+            x += 10;
+        }
+        
+        drx = r.x;
+        
+        if(!generations.containsKey(generation))
+        {
+            generations.put(generation, new ArrayList<>());
+        }
+        if(!generations.get(generation).contains(r))
+        {
+            generations.get(generation).add(r);
+        }
+        
+        return r;
+    }
+    
+    private boolean generationContainsIntersectingRect(int generation, Rect r) {
+        if(!generations.containsKey(generation)) return false;
+        
+        ArrayList<Rect> gen = generations.get(generation);
+        
+        for(int i=0; i<gen.size(); ++i)
+        {
+            Rect r2 = gen.get(i);
+            if((r.x > r2.x && r.x < r2.x + r2.width) ||
+               (r.x + r.width > r2.x && r.x + r.width < r2.x + r2.width) ||
+               (r2.x > r.x && r2.x < r.x + r.width) ||
+               (r2.x + r2.width > r.x && r2.x + r2.width < r.x + r.width))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    private void plotDwarfDescendancy(Graphics g, Dwarf dwarf, int parent_x, int parent_y, int generation)
+    {
+        if(dwarf.getChildren().isEmpty()) return;
+        
+        FontMetrics fontMetrics = g.getFontMetrics();
+        
+        int numChildren = dwarf.getChildren().size();
+        int reservedWidth = 0;
+        for(int i=0; i<numChildren; ++i)
+        {
+            reservedWidth += fontMetrics.stringWidth(dwarf.getChildren().get(i).getCasedName()) + 40; // 10 padding + 10 margin
+        }
+        
+        int initial_x = parent_x - reservedWidth/2 + fontMetrics.stringWidth(dwarf.getCasedName())/2;
+        
+        for(int i=0; i<numChildren; ++i)
+        {
+            int y = parent_y + fontMetrics.getHeight()*2 + 20;
+            Rect r = plotDwarfName(g, dwarf.getChildren().get(i), initial_x, y, generation + 1);
+            plotDwarfDescendancy(g, dwarf.getChildren().get(i), r.x, y, generation + 1);
+            initial_x = r.x + r.width;
+        }
     }
 }
